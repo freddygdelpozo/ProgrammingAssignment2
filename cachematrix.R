53a92f657a6299615740209af2a82746ca262b33
freddygdelpozo <f.delpozo.leon@gmail.com>
2014-09-21 09:32:56 +0200

    Wrote a function for the Assignment
    R function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R-programming Assignment week 3
 
-## Write a short comment describing this function
+## Assignment about lexical scooping
+## consice explanation
+## https://class.coursera.org/rprog-007/forum/thread?thread_id=707
 
-makeCacheMatrix <- function(x = matrix()) {
+library(MASS)
 
+makeCacheMatrix <- function(r_matrix = matrix()) {
+  
+  ma <- NULL
+  set <- function(y){
+    r_matrix <<- y
+    ma <<- NULL
+    
+  }
+  
+  get <- function() r_matrix
+  
+  setmatrix <- function(solve) 
+    ma <<- solve
+  
+  getmatrix <- function() ma
+  
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## chol2inv(chol(data)) is faster than solve algorithm, first
+## computes the factors with chol() and then the inverse
+## with chol2inv()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(r_matrix, ...) {
+        ## Return the inverse of 'x'
+  inverse_m  <- r_matrix$getinverse()
+  
+  if (!is.null(inverse_m)){
+    message("got cached data")
+    return(inverse_m)
+  }
+  
+  data  <- r_matrix$get()
+  inverse_m  <- chol2inv(chol(data))
+  r_matrix$setinverse(inverse_m)
+  inverse_m
+  
 }
